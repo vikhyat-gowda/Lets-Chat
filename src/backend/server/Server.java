087b22e82e121da9477ccdee2c89e974b43666ab
vikhyat-gowda <vikhyat.gowda1@gmail.com>
2023-02-13 14:46:57 +0530

    feat: added arguments cli arguments to server

--- src/backend/server/Server.java
+++ src/backend/server/Server.java
@@ -11,6 +11,7 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Server implements Runnable{
 
@@ -21,6 +22,7 @@ public class Server implements Runnable{
     private Thread manage;
     private Thread receive;
     private final int MAX_ATTEMPTS = 5;
+    private boolean raw = false;
 
     private final List<ClientInfo> connectedClients = new ArrayList<ClientInfo>();
     private List<Integer> clientResponse = new ArrayList<>();
@@ -44,6 +46,67 @@ public class Server implements Runnable{
         System.out.println("Server Started on port " + port);
         manageClients();
         receive();
+        Scanner scanner = new Scanner(System.in);
+
+        while (running) {
+            String text = scanner.nextLine();
+            if(!text.startsWith("/")) {
+                sendToAll(PacketType.MESSAGE, "Server: " + text);
+                continue;
+            }
+            text = text.substring(1);
+            if(text.equals("raw"))
+                raw = !raw;
+            else if (text.equals("list")) {
+                System.out.println("================\nClients");
+                for (int i =0; i < connectedClients.size(); i++) {
+                    ClientInfo c = connectedClients.get(i);
+                    System.out.printf("Name: %s Id: %d Address: %s Port: %d\n", c.getName(), c.getID(),c.getAddress().toString(), c.getPort());
+                }
+                System.out.println("================");
+            }
+            else if (text.startsWith("kick")) {
+                System.out.println("here1");
+                String name = text.split(" ")[1];
+                boolean isNum = true;
+                int id = -1;
+
+                try {
+                    System.out.println("here2");
+                    System.out.println("text "+ name);
+                    id = Integer.parseInt(name);
+                }catch (NumberFormatException e) {
+                    System.out.println("here3");
+                    isNum = false;
+                }
+
+                if(isNum == true) {
+                    System.out.println("here4");
+                    boolean exist = false;
+                    for (int i = 0; i < connectedClients.size(); i++) {
+                        if(connectedClients.get(i).getID() == id){
+                            exist = true;
+                            break;
+                        }
+                    }
+                    if(exist) {
+                        System.out.println("here5");
+                        disconnect(id, true);
+                    }else {
+                        System.out.println("Client ID "+ id + " doesn't exit");
+                    }
+                }
+                else  {
+                    for (int i = 0; i < connectedClients.size(); i++) {
+                       ClientInfo c = connectedClients.get(i);
+                       if(name.equals(c.getName())) {
+                           disconnect(c.getID(), true);
+                           break;
+                       }
+                    }
+                }
+            }
+        }
     }
 
     private void receive() {
@@ -69,6 +132,7 @@ public class Server implements Runnable{
     private void processPacket(DatagramPacket packet) {
 
         String message = new String(packet.getData());
+        if (raw) System.out.println(message);
         if(message.startsWith("/c/")) {
             int id = UniqueIdentifier.getIdentifier();
             connectedClients.add(new ClientInfo(message.split("/c/|:|/e/", 0)[1], packet.getAddress(), packet.getPort(), id));
@@ -76,11 +140,11 @@ public class Server implements Runnable{
             send(PacketType.CONNECTION,Integer.toString(id) , packet.getAddress(), packet.getPort());
         }
         else if (message.startsWith("/m/")) {
-            System.out.println(message.split("/m/|/e/")[1]);
+//            System.out.println(message.split("/m/|/e/")[1]);
             sendToAll(PacketType.MESSAGE,message.split("/m/|/e/")[1]);
         }
         else if (message.startsWith("/d/")) {
-            System.out.println(message.split("/d/|/e/")[1]);
+//            System.out.println(message.split("/d/|/e/")[1]);
             disconnect(Integer.parseInt(message.split("/d/|/e/")[1]), true);
         } else if (message.startsWith("/i/")) {
             clientResponse.add(Integer.parseInt(message.split("/i/|/e/")[1]));
@@ -104,7 +168,7 @@ public class Server implements Runnable{
                     sendToAll(PacketType.PING, "server");
                     try {
                         Thread.sleep(5000);
-                        System.out.println("Ping Started");
+
                     }catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -122,7 +186,6 @@ public class Server implements Runnable{
                             clientResponse.remove(Integer.valueOf(c.getID()));
                         }
                     }
-                    System.out.println("Ping Ended");
                 }
             }
         };
@@ -160,21 +223,30 @@ public class Server implements Runnable{
     }
 
     private void disconnect(int id, boolean status) {
+
         System.out.println("called disconnected ");
         ClientInfo c = null;
+
+        boolean exist = false;
+
         for (int i = 0; i < connectedClients.size(); i++) {
             if(connectedClients.get(i).getID() == id) {
                 c = connectedClients.get(i);
                 connectedClients.remove(i);
+                exist = true;
                 break;
             }
         }
+
         String message = "";
-        if(status){
+
+        if(!exist) return;
+        if (status) {
             message = String.format("Client: %s (%d) @ %s port %d disconnected", c.getName(), c.getID(), c.getAddress().toString(), c.getPort());
-        }else {
+        } else {
             String.format("Client: %s (%d) @ %s port %d timed out", c.getName(), c.getID(), c.getAddress().toString(), c.getPort());
         }
+
         System.out.println(message);
     }
 
